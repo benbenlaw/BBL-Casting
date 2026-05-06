df8ca74f87862a8291b74b2a40d49d471351269b
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-05-06 16:10:40 +0100

    2.3.11

--- src/main/java/com/benbenlaw/casting/mixin/ItemStackMixin.java
+++ src/main/java/com/benbenlaw/casting/mixin/ItemStackMixin.java
@@ -1,13 +1,17 @@
 package com.benbenlaw.casting.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Consumer;
 
 import static com.benbenlaw.casting.item.EquipmentModifier.UNBREAKING;
 
@@ -15,14 +19,15 @@ import static com.benbenlaw.casting.item.EquipmentModifier.UNBREAKING;
 public class ItemStackMixin {
 
     @Inject(
-            method = "hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/EquipmentSlot;)V",
+            method = "hurtAndBreak(ILnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;Ljava/util/function/Consumer;)V",
             at = @At("HEAD"),
             cancellable = true
     )
     private void casting$customDurabilityHandling(
             int amount,
+            ServerLevel level,
             LivingEntity entity,
-            EquipmentSlot slot,
+            Consumer<Item> onBreak,
             CallbackInfo ci
     ) {
         ItemStack self = (ItemStack)(Object)this;
@@ -30,13 +35,14 @@ public class ItemStackMixin {
         if (!self.isDamageableItem()) return;
 
         if (self.getComponents().has(UNBREAKING.dataComponent.get())) {
-            int level = (int) self.getOrDefault(UNBREAKING.dataComponent.get(), 0);
-            float chance = level * 0.1f;
 
-            RandomSource random = (entity != null) ? entity.getRandom() : RandomSource.create();
+            int lvl = (int) self.getOrDefault(UNBREAKING.dataComponent.get(), 0);
+            float chance = lvl * 0.1f;
+
+            RandomSource random = entity.getRandom();
 
             if (random.nextFloat() < chance) {
-                ci.cancel(); // Cancel the durability loss
+                ci.cancel(); // fully prevents durability loss
             }
         }
     }
